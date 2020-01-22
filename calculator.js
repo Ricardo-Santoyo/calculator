e4fef08ab5c3c87bd0e1433725bb475c042f2385
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-01-22 15:35:27 -0600

    added function operate(a, b, operator) which takes two numbers and a operator and returns a result

--- calculator.js
+++ calculator.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case 'add':
+            const sum = add(a, b);
+            return sum;
+        case 'subtract':
+            const difference = subtract(a, b);
+            return difference;
+        case 'multiply':
+            const product = multiply(a, b);
+            return product;
+        case 'divide':
+            const quotient = divide(a, b);
+            return quotient;
+    }
+}
