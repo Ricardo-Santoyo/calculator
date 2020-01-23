7c7f93d499e7fea61b5941ab0c7d8cef0e1c9a9f
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-01-22 20:46:15 -0600

    added function displayNum(e) and function displayOperators(e  which displays a  number  or  an  operator  on the screen depending on which button is pressed.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+const display = document.querySelector('#display');
+const operators = document.querySelector('#operators');
+const numpad = document.querySelector('#numpad');
+let defaultText = 0;
+    display.textContent = defaultText;
+let a = "";
+let b = "";
+let operator = "";
+
 function add(a, b) {
     return a + b;
 }
@@ -28,5 +37,52 @@ function operate(a, b, operator) {
         case 'divide':
             const quotient = divide(a, b);
             return quotient;
+    }
+}
+
+operators.addEventListener('click', displayOperators);
+
+function displayOperators(e) {
+    let operatorSelected;
+    operatorSelected = e.target.id;
+    switch (operatorSelected) {
+        case "+":
+        case "−":
+        case "×":
+        case "÷":
+            display.textContent += operatorSelected;
+            operator += operatorSelected;
+            break;
+    }
+
+}
+
+numpad.addEventListener('click', displayNum)
+
+function displayNum(e) {
+    if (display.textContent == 0) {
+        display.textContent = "";
+    }
+    let num;
+    num = e.target.id
+    switch (num) {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case "0":
+            display.textContent += num;
+            if (operator != "") {
+                b += num;
+            }
+            else if (operator == "") {
+                a += num;
+            }
+            break;
     }
 }
